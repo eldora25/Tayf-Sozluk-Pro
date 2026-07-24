419e196b64ea1d7d99bf07eed195bbfe0ecbf16e
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-25 01:23:17 +0300

    Implement MainActivity for Flutter v2 embedding
    Removed manual GeneratedPluginRegistrant code as it's no longer needed with v2 embedding.

---
+++ android/app/src/main/kotlin/com/tayfunyamak/tayfsozlukpro/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.tayfunyamak.tayfsozlukpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // v2 embedding ile birlikte içerideki manuel GeneratedPluginRegistrant kodlarına gerek kalmadı.
+    // Flutter bunu arka planda otomatik olarak bağlamaktadır.
+}
