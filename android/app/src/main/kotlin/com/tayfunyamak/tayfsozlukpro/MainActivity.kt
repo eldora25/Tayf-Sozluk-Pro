df1c2c4966ae84a98e29b17b41939c5932ea5630
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-25 04:04:37 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/tayfunyamak/tayfsozlukpro/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.tayfunyamak.tayfsozlukpro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // Bu modern v2 sınıfı tamamen boştur. 
-    // Tüm eklenti (TTS, SharedPreferences vb.) bağlantıları Flutter motoru tarafından otomatik yönetilir.
-}
