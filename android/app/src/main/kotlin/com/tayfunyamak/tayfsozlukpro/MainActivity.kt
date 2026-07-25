d4e1e77ce52f33c5286e298c455b657cb48c9554
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-25 03:34:41 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/tayfunyamak/tayfsozlukpro/MainActivity.kt
+++ android/app/src/main/kotlin/com/tayfunyamak/tayfsozlukpro/MainActivity.kt
@@ -3,6 +3,6 @@ package com.tayfunyamak.tayfsozlukpro
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // İçerisinde hiçbir eski v1 metodu veya manuel PluginRegistry kodu barındırmaz.
-    // Flutter v2 embedding yapısı tüm eklentileri arka planda yerel olarak bağlar.
+    // Bu modern v2 sınıfı tamamen boştur. 
+    // Tüm eklenti (TTS, SharedPreferences vb.) bağlantıları Flutter motoru tarafından otomatik yönetilir.
 }
