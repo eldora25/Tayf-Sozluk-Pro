ce1837ccac10d14d946009aa014e226bcc6f81f4
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-07-25 06:17:09 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/tayfunyamak/tayfsozlukpro/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.tayfunyamak.tayfsozlukpro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // Saf V2 entegrasyonu, içi tamamen boş kalmalıdır.
-}
