cc08fa3b527b12c4fa1d2f8950cb0471cc87eac9
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-08-07 23:26:13 +0300

    Remove TTS Manager registration from setupLocator
    Removed TTS Manager registration from locator setup to prevent type errors.

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,10 +1,8 @@
 import 'package:get_it/get_it.dart';
-import 'tts_manager.dart';
 
 final GetIt locator = GetIt.instance;
 
 void setupLocator() {
-  // TTS Manager'ı Singleton olarak sisteme enjekte ediyoruz
-  // İlerleyen aşamalarda FirebaseSyncService gibi servisler de buraya eklenecek.
-  locator.registerLazySingleton<TtsManager>(() => TtsManager());
+  // TTS global instance (globalTts) ile yönetildiği için buradan kaldırıldı (type hatası önlendi).
+  // İleride Firebase/Sentry servisleri buraya eklenecek.
 }
